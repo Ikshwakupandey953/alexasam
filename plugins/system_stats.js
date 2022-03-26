38dc04ada8396066bf2a0bdf309e57111a3c8d47
Ikshwakupandey953 <98660511+Ikshwakupandey953@users.noreply.github.com>
2022-03-26 14:26:13 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -20,7 +20,7 @@ const Lang = Language.getString('system_stats');
             
             var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
        
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "\n\n*Hii,This is Jarvis....*\n*💫🧚‍♀️Hey There Im Alive Now*\n\n*◉Version :► 8.5.0 (Public Release)*\n*◉Branch  :► Owner*\n*◉About :► Jarvis Is a Powerfull Bot For Whatsapp.*\n*●Grade :►Dev"})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "\n\n*Hii,This is Jarvis....*\n*💫🧚‍♀️Hey There Im Alive Now*\n\n*◉Version :► 8.5.0 (Private Release)*\n*◉Branch  :► Owner*\n*◉About :► Jarvis Is a Powerfull Bot For Whatsapp.*\n●Grade :►Dev"})
 
     }
     else {
