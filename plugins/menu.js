ea07ee82563aeac72bbcfe6c73f0395b3c8f4b07
Ikshwakupandey953 <98660511+Ikshwakupandey953@users.noreply.github.com>
2022-03-26 22:25:24 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -18,7 +18,7 @@ var ggg = Buffer.from(clh.cd, 'base64')
 
 var ddd = ggg.toString('utf-8')
 
-Abu.addCommand({pattern: 'menu', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+Neotro.addCommand({pattern: 'menu', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 
 // send a list message!
 
